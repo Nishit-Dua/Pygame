91f4f4791b6122ec6e04f2d50bbff862f5f8dbf1
Nishit-Dua <nishitdua175@gmail.com>
2020-05-24 23:36:19 +0530

    moved var  to assets and renamed main.py

--- Zombie/assets.py
+++ Zombie/assets.py
@@ -10,6 +10,16 @@ x, y = 0, (winSize-80 - 46)
 height, width = 80, 80
 
 
+walkcount = 0
+LEFT = False
+RIGHT = False
+jump = False
+
+jumpPower = 10
+neg = 1
+velocity = 10
+
+
 for n in range(1, 11):
     movingRight.append(
         scale(pygame.image.load(f'assets/Walk{n}.png'), (height, width)))
